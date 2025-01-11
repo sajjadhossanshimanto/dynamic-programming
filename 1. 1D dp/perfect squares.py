916448313b3b35251f2f0daf55e7aedd32d7a1aa
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-12 00:49:50 +0600

    Lagrange theorem

--- 1. 1D dp/perfect squares.py
+++ 1. 1D dp/perfect squares.py
@@ -29,3 +29,41 @@ s.numSquares(12)
 # 2
 s.numSquares(13)
 # %%
+from math import isqrt
+
+class Solution:
+    # all numbers can be found as the sums of squares. Lagrange proved that all could be found as the sum of four or less
+    # so, this should never equal anything larger than four; it's just a matter of finding out when and by how much
+    def numSquares(self, n: int) -> int:
+        # simple integer arithmatic to avoid floating point issues
+        def perfect_square(n: int) -> bool:
+            return n == isqrt(n)**2
+
+        # check all numbers between 1 and sqrt(n) to see if square-pair exists
+        def brute_double(n: int) -> bool:
+            for i in range(1, isqrt(n) + 1):
+                if perfect_square(n - i**2):
+                    return True
+            return False
+
+        # https://en.wikipedia.org/wiki/Legendre%27s_three-square_theorem
+        def legandre_triple(n: int) -> bool:
+            while (n % 4 == 0):
+                n //= 4
+            return (n % 8 != 7)
+
+        # perfect square case
+        if perfect_square(n):
+            return 1
+
+        # sum of two squares case
+        elif brute_double(n):
+            return 2
+
+        # equals three case
+        elif legandre_triple(n):
+            return 3
+
+        # hardest mathematically, but fortunately we don't have to include it
+        else:
+            return 4
