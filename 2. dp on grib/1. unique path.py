22a9045097aaf998a82458cc6b55427631587afe
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-11 14:53:49 +0600

    recurtion time complexity analysis

--- 2. dp on grib/1. unique path.py
+++ 2. dp on grib/1. unique path.py
@@ -29,7 +29,24 @@ class Solution:
             return dfs(x+1, y) + dfs(x, y+1)
 
         return dfs(0, 0)
-
+'''
+time complexity: 2^n
+space complexity: O(mn) + O((m-1)+(n-1))
+    at max m*n caching + recurtion space
+    - for a m*n matrix
+    |---------
+    | x x x x
+    | x x x x
+    | x x x x
+    our recursion will go 
+        - down down down ↓ until end is reached 
+        - then we will go → right and down. but no downward cell. so continue to go right and right
+    |---------      |---------
+    | x x x x       | x x x x
+    | ↓ x x x       | ↓ x x x
+    | ↓ x x x       | ↓ → → x 
+    so at max n-1 downward and m-1 right ward
+'''
 s = Solution()
 # %%
 # 28
