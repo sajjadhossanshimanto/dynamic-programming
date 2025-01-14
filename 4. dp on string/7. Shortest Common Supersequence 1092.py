e7f4a6b680d0b8b06cf79e6c8843b5a946afd1fb
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-15 00:18:04 +0600

    faulty try

--- 4. dp on string/7. Shortest Common Supersequence 1092.py
+++ 4. dp on string/7. Shortest Common Supersequence 1092.py
@@ -18,6 +18,59 @@ taking this decition is the easy part. the main hard part isto generate the stri
 from collections import deque
 
 class Solution:
+    # faulty code
+    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
+        row, column = len(str1), len(str2)
+        dp = [[0]*(column) for _ in range(row)]
+        
+        for i in range(row):
+            for j in range(column):
+                if str1[i]==str2[j]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        
+        # return dp
+
+        match = deque()
+        i, j = row-1, column-1
+        while i>=0 and j>=0:
+            if str1[i]==str2[j]:
+                match.appendleft(str1[i])
+                i, j = i-1, j-1# go diagonal
+            else:
+                # go to the cell with dp[i][j] having max value
+                # if both of (i+1, j) (i, j+1) cointains same value we can go to any of them
+                if dp[i-1][j]>=dp[i][j-1]:
+                    match.appendleft(str1[i])
+                    i, j = i-1, j
+                else:
+                    match.appendleft(str2[j])
+                    i, j = i, j-1
+        
+        while i>-1:
+            match.appendleft(str1[i])
+            i-=1
+        
+        while j>-1:
+            match.appendleft(str2[j])
+            j-=1
+        
+        return "".join(match)
+
+
+s = Solution()
+# %%
+# "cabac"
+s.shortestCommonSupersequence(str1 = "abac", str2 = "cab")
+# %%
+# bgruoote
+s.shortestCommonSupersequence("brute", "groot")
+# %%
+'theres no way amound. must use a extra row'
+from collections import deque
+
+class Solution:
     def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
         row, column = len(str1), len(str2)
         dp = [[0]*(column+1) for _ in range(row+1)]
@@ -57,12 +110,3 @@ class Solution:
         
         return "".join(match)
 
-
-s = Solution()
-# %%
-# "cabac"
-s.shortestCommonSupersequence(str1 = "abac", str2 = "cab")
-# %%
-# bgruoote
-s.shortestCommonSupersequence("brute", "groot")
-# %%
