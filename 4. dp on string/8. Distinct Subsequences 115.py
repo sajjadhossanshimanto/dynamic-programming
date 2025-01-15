a208007dc0e3c9ecdd28d2c42e19a28651be59fa
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-15 19:02:44 +0600

    ac with memoization. counting leaf problem

--- 4. dp on string/8. Distinct Subsequences 115.py
+++ 4. dp on string/8. Distinct Subsequences 115.py
@@ -7,19 +7,19 @@ class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         
         # counting leaf problem
-        count = [0]
+        @lru_cache(None)
         def backtrack(i=0, j=0):
             if j==len(t):
-                count[0]+=1
-                return
-            if i==len(s): return 
+                return 1
+            if i==len(s): return 0
 
+            l, r = 0, 0
             if s[i]==t[j]:
-                backtrack(i+1, j+1)
-            backtrack(i+1, j)
+                l = backtrack(i+1, j+1)
+            r = backtrack(i+1, j)
+            return l+r
     
-        backtrack()
-        return count[0]
+        return backtrack()
 
 s = Solution()
 # 3
