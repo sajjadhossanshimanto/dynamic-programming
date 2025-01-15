acc2cc43e08155d5a6e12838496433f48321d357
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-15 19:38:07 +0600

    dp implimentation

--- 4. dp on string/8. Distinct Subsequences 115.py
+++ 4. dp on string/8. Distinct Subsequences 115.py
@@ -28,3 +28,43 @@ s.numDistinct(s = "rabbbit", t = "rabbit")
 # 5
 s.numDistinct(s = "babgbag", t = "bag")
 # %%
+'''
+dp implimentation
+'''
+
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        '''
+        ----->s (let i represent s)
+        |
+        |
+        t (let j represent t)
+        '''
+        dp = [[0]*(len(s)+1) for _ in range(len(t))]
+        
+        # step 1. write base case
+        # which is row wise already initialised as 0
+        dp.append([1]*(len(s)+1))
+        
+        # step 2 -> loop
+        # loop must run oposite of recurtion direction
+        for i in range(len(t)-1, -1, -1):
+            for j in range(len(s)-1, -1, -1):
+                # step 3 -> copy paste recurtion
+                l, r = 0, 0
+                # print(i, j)
+                if t[i]==s[j]:
+                    l = dp[i+1][j+1]
+                r = dp[i][j+1]
+                '''the above is not direct copy paste. because i think 
+                i have supposed the t in column wise in recurtion but in row wise here'''
+                dp[i][j] = l+r
+            
+        return dp
+
+s = Solution()
+# 3
+s.numDistinct(s = "rabbbit", t = "rabbit")
+# %%
+s.numDistinct(s = "babgbag", t = "bag")
+# %%
