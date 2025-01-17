0591600ff939494ce580d64c88baf5763c60414f
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-17 16:12:27 +0600

    recurtion approach gets memor limit out

---
+++ 5. dp on stock/2. ii Best Time to Buy and Sell Stock .py
@@ -0,0 +1,52 @@
+'''
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+can perform multiple buy and sell but
+can't buy again berfor sell. so we have to buy sell then buy sell
+
+1. whenever there are `a lot of ways`.
+2. we `try all ways`. and 
+3. `find the best way`
+whenever we say try all ways -> we mean `recurtion`
+'''
+#%%
+from typing import List
+from functools import lru_cache
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        @lru_cache(None)
+        def recursion(start=0, bought=False, buy_index=None) -> int:
+            if start==len(prices):
+                return 0
+            
+            if bought :
+                # sell it here or not sell here
+                return max(
+                    0,
+                    recursion(start+1, False) + prices[start] - prices[buy_index],
+                    recursion(start+1, bought, buy_index)
+                )
+            else:
+                # buy here or not buy
+                return max(
+                    0,
+                    recursion(start+1, True, start),
+                    recursion(start+1, bought, buy_index)
+                )
+        
+        return recursion()
+
+s = Solution()
+# %%
+# 7
+s.maxProfit(prices = [7,1,5,3,6,4])
+# %%
+# 4
+s.maxProfit(prices = [1,2,3,4,5])
+# %%
+# 0
+s.maxProfit([7,6,4,3,1])
+# %%
