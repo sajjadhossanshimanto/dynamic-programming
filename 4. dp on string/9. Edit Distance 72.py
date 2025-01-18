653a4bbcb66cfc9632707d32456cee09564b5ef4
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-18 15:25:35 +0600

    solution from lecture gets ac

--- 4. dp on string/9. Edit Distance 72.py
+++ 4. dp on string/9. Edit Distance 72.py
@@ -10,89 +10,40 @@ if we have a lcs -> ...r..s.. we need to count intermediate char
 as we can perform replace operation here
 
 
-TODO: what if word2 > word1
 '''
 #%%
-def longestCommonSubsequence(text1: str, text2: str) -> int:
-    row, column = len(text1), len(text2)
-    dp = [[0]*(column+1) for _ in range(row+1)]
-
-    for i in range(row-1, -1, -1):
-        for j in range(column-1, -1, -1):
-            if text1[i]==text2[j]:
-                dp[i][j] = 1 + dp[i+1][j+1]
-            else:
-                dp[i][j] = max(dp[i+1][j], dp[i][j+1])
-
-    # return dp
-    match = []
-    i, j = 0, 0
-    while i!=row and j!=column:
-        if text1[i]==text2[j]:
-            match.append((i, j))
-            i, j = i+1, j+1
-        else:
-            if dp[i+1][j]>=dp[i][j+1]:
-                i, j = i+1, j
-            else:
-                i, j = i, j+1
-    
-    return match
+from functools import lru_cache
 
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        cummon =  longestCommonSubsequence(word1, word2)
-        # left part
-        # if cummon[0][0]>cummon[0][1]:
-        #     '''
-        #     this signifies source word has more char than target word on the left side of their first match
-        #     so cummon part will be replaced + removal from source
-        #     removal count -> their diff
-        #     '''
-        #     left = cummon[0][0] - cummon[0][1]
-        #     left += cummon[0][0] - left
-        # else:
-        #     "insertion + replasement"
-        left = abs(cummon[0][0] - cummon[0][1])
-
-        # right part
-        right = abs(cummon[-1][0] - cummon[-1][1])
-
-        # middle
-        # cummon[-1] - cummon[0] + 1
-        '''
-            cummon[-1][1] - cummon[0][1] + 1 - len(cummon)
-        that much char are extra in text2 what does not matches with text1
-        - should i rplace or perform remove+insert
-        - how much can i replace
-        '''
-        word2_extra = cummon[-1][1] - cummon[0][1] + 1 - len(cummon)
-        word1_extra = cummon[-1][0] - cummon[0][0] + 1 - len(cummon)
         
-        # replacement                + insertion
-        if word2_extra > word1_extra:
+        # "node counter problem"
+        @lru_cache(None)
+        def recursion(i=0, j=0):
+            if j==len(word2):
+                "matched"
+                return len(word1) - i
+            if i==len(word1):
+                # requi insertion operation
+                return len(word2) - (j)
+            
+            # logic
+            if word1[i]==word2[j]:
+                # no operation needs to perform
+                return 0 + recursion(i+1, j+1)
+        
+            # insertion
+            insert = 1 + recursion(i, j+1)# hypothetically one char is inserted matching j
+            '''deletion:
+            after not matching one char is deleted from source word but we are still looking for j
             '''
-            word2 -> target word
-            target_word has more extra char then
-            word1 needs insertion to match word2
-            + common part can be replaced
-            num of insertion -> is their diff
-            '''
-            middle = word1_extra + (word2_extra - word1_extra)
-        else:
-            '''
-            source word has less extra char then target so
-            word1 needs removal + replacement to match word2
-            count of removal -> is their difference
-            '''
-            # replacement + removal
-            middle = word2_extra + (word1_extra-word2_extra)
-        middle = min(word1_extra, word2_extra) + abs(word1_extra-word2_extra)
+            remove = 1 + recursion(i+1, j)
+            # replace
+            rep = 1 + recursion(i+1, j+1)
+            return min(insert, remove, rep)
+        
+        return recursion()
 
-
-        return left, middle, right
-        # return longestCommonSubsequence(word1, word2)
-        return left + middle + right
 
 s = Solution()
 # %%
@@ -101,13 +52,4 @@ s.minDistance(word1 = "horse", word2 = "ros")
 # %%
 # 5
 s.minDistance(word1 = "intention", word2 = "execution")
-'''
-out: 7 = (3, 4, 0) but things are not like that
-ans: 5
-Explanation: 
-intention -> inention (remove 't')
-inention -> enention (replace 'i' with 'e')
-enention -> exention (replace 'n' with 'x')
-exention -> exection (replace 'n' with 'c')
-exection -> execution (insert 'u')'''
 # %%
