baa6903422629464134d2d424c88db33b00b124e
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-18 18:43:35 +0600

    dp solution

--- 4. dp on string/10. Wildcard Matching 44.py
+++ 4. dp on string/10. Wildcard Matching 44.py
@@ -51,3 +51,41 @@ s.isMatch("abcde", "a?c*de")
 # failed test case
 s.isMatch("", "***")
 # %%
+# dp solution
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        '''
+        as i represents s. s will be in the x direction
+        |----> p
+        |
+        |
+        s
+        '''
+        row, column = len(s), len(p)
+        dp = [[False]*(column+1) for _ in range(row+1)]
+
+        " base cases "
+        # if i==len(s) and j==len(p):
+        dp[-1][-1] = True
+        # if j>=len(p): return False
+        for i in range(row):
+            dp[i][-1] = False
+
+        for j in range(column-1, -1, -1):
+            if p[j]!="*": break
+            dp[-1][j] = True
+
+        for i in range(len(s)-1, -1, -1):
+            for j in range(len(p)-1, -1, -1):
+                if p[j]=="?" or s[i]==p[j]:
+                    dp[i][j] = dp[i+1][j+1]
+                elif p[j]=="*":
+                    dp[i][j] = dp[i+1][j] or dp[i][j+1]
+        
+        return dp
+        return dp[0][0]
+
+s=Solution()
+s.isMatch("ray", "?ay")
+# %%
