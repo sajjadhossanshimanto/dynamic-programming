0363f30648591194fb9aaaa26b636a2abc12c166
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-18 18:20:44 +0600

    pick and non pick for *

--- 4. dp on string/10. Wildcard Matching 44.py
+++ 4. dp on string/10. Wildcard Matching 44.py
@@ -25,9 +25,13 @@ class Solution:
             if p[j]=="?" or s[i]==p[j]:
                 return backtrack(i+1, j+1)
             if p[j]=="*":
-                for i in range(i, len(s)+1):
-                    if backtrack(i, j+1): return True
-            
+                # for i in range(i, len(s)+1):
+                #     if backtrack(i, j+1): return True
+                '''same thing in pick and not pick approach
+                😯 somehow this performs way much better
+                '''
+                return backtrack(i+1, j) or backtrack(i, j+1)
+
             return False
         
         return backtrack()
@@ -39,6 +43,10 @@ s.isMatch("ray", "?ay")
 s.isMatch("abcdef", "ab**ef")
 # %%
 s.isMatch("abcd", "**abcd")
+#%%
+s.isMatch("abcde", "a?c*e")
+#%%
+s.isMatch("abcde", "a?c*de")
 # %%
 # failed test case
 s.isMatch("", "***")
