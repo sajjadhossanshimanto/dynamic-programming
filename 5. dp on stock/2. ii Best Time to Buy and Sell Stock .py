50e3e29f7be33d70853c4a648b2f295936cb19fe
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-18 20:26:01 +0600

    solution from neetcode

--- 5. dp on stock/2. ii Best Time to Buy and Sell Stock .py
+++ 5. dp on stock/2. ii Best Time to Buy and Sell Stock .py
@@ -149,6 +149,44 @@ class Solution:
 s = Solution()
 s.maxProfit(prices = [7,1,5,3,6,4])
 # %%
+'''
+
+stock prices are best represented when we draw graps
+7| .(7)
+6| \            .(6)
+5|  \   (5).   / \
+4|   \    / \ /   .(4)
+3|    \  /   .(3)
+2|     \/
+1|      .(1)
+ |______________________
+0 
+
+here we have advantage that we can predict the fiture.
+seeing the future we will decide weighter to buy in previous day or not
+as we just have to return the max profit no mattr how we does that
+
+on the 1st day the price is 7 but we see the next moment price is gooing tobe down. so we will not buy
+the next day price is 1 but it will be increased the next day. so buy here
+next day the price is increased we will hold as per as it increases. but next day it will the decreased so we sell here.
+
+thats that we need to do again and again. buy in less sell in heigh price.
+if we follow that trend we will notice that we will have sell in (1->5) (3->6)
+thats means in every increasing part of the graph
+
+thus our result is the sum of all increasing part of the graph.
+'''
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        for i in range(1, len(prices)):# as 0'th index does not have any previous price
+            if prices[i]>prices[i-1]:
+                profit += (prices[i] - prices[i-1])
+        
+        return profit
+
+#%%
 # solution from leetcode 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
