08342397c47cf06dbad0b9f32c6ccdd7efd99bf3
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-28 17:07:32 +0600

    problem stetment & approach

---
+++ 3. sub secquence dp/6. 0_1 knapsack.py
@@ -0,0 +1,26 @@
+'''
+very very important question
+this is the one that will come to mind first if an inverviewer wants to ask from dp
+
+
+input -> 2 array + max_weight
+wt  -> 3   4   5
+val -> 30  40  60
+my max capacity of taking weight is -> 8
+what is the max value i can with max weight 8
+
+1st approach that will come to mind -> lets be greedy
+take the most costly items first. but this will not actually work
+
+wt  -> 3   2   5
+val -> 30  40  60
+max_weight -> 6
+if we still 60 with most value left capacity is 6-5=1 nothing more to take
+but we can actually better value of 40+30 with wt 3+2
+
+this happens due to lack of uniformaty. values and weight are not uniform
+
+so we have seen max value may come from from items with least value. so there is no certain way
+so try out all ways choise the best ooption
+'''
+#%%
