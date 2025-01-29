e7dbe9e5656e0e6986ea3d125966e6b2b884f468
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-01-29 10:52:12 +0600

    up & down stream implimentation

--- 3. sub secquence dp/8. i coin change 322.py
+++ 3. sub secquence dp/8. i coin change 322.py
@@ -14,6 +14,7 @@ from functools import lru_cache
 
 inf = float("inf")
 class Solution:
+    # down stream only
     def coinChange(self, coins: List[int], amount: int) -> int:
         coins.sort()
 
@@ -35,7 +36,33 @@ class Solution:
         return -1 if min_node[0]==inf else min_node[0]
 
 s = Solution()
-# %%
+# %% up & down stream
+inf = float("inf")
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        coins.sort()
+
+        # node counting problem
+        @lru_cache(None)
+        def dfs(start=0, left=amount):
+            if left==0: 
+                return 0
+            if start==len(coins): return inf
+            
+            "node count will not increase for not pick"
+            not_pick = dfs(start+1, left)
+            pick = inf
+            if left-coins[start]>=0:
+                "replacements are allowed"
+                pick = 1 + dfs(start, left-coins[start])# pick
+            
+            return min(pick, not_pick)
+
+        ans = dfs()
+        return -1 if ans==inf else ans
+
+s = Solution()
+#%%
 # 3
 s.coinChange(
     coins = [1,2,5], amount = 11
@@ -62,3 +89,5 @@ s.coinChange(
     [3,7,405,436],
     8839
 )
+# or maximum recurtion dept will excid as recurtion dept is 3000
+# %%
