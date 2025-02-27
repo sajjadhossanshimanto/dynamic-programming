ba2d4e55c40846c22548c8cd92f3c50cefb5e9fa
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-02-27 10:47:46 +0600

    build dp  but not working

--- 5. dp on stock/3. iii Best Time to Buy and Sell Stock.py
+++ 5. dp on stock/3. iii Best Time to Buy and Sell Stock.py
@@ -85,5 +85,57 @@ class Solution:
 
         return recurtion()
 
+'''
+gets ac but not optimal. took almost 2 sec to complete
+'''
+
+s = Solution()
+# %% dp solution 
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        '''
+        ## setp 1 -> declear dp array
+        def recurtion(start=0, brought=False, cap=2):
+        - 3 parameters are there. so we can make a 3D dp
+        
+        so dp[start][brought][cap]
+        - `start` goes from 0 to n. 
+        - `brought` can be 0 or 1.
+        - `cap` can be 0, 1, 2.
+
+        ## step 2 -> set defaule values.
+        - befault value is set depending upon `base case`
+        - return value is the max profix. which is set to default 0
+        - everything else is indicated by index
+
+        ## step 3 -> run loops in oposite direction
+        
+        ## step 4 -> copy paste logics
+        
+        ## step 5 -> return value
+        start=0, brought=False, cap=2
+        '''
+        n = len(prices)
+        dp = [[[0]*3 for _ in range(2)] for _ in range(n+1)]
+
+        for start in range(n-1, -1, -1):
+            for brought in range(2):
+                for cap in range(2+1):# during implementation i've noticed for cap we need cap-1. so the loop should run increasing oder
+                    if brought:
+                        # we have 2 choices either to sell or not
+                        dp[start][brought][cap] = max(
+                            prices[start] + dp[start+1][0][cap-1],
+                            dp[start+1][1][cap]# hold
+                        )
+                    else:
+                        # we have 2 choices either to buy or not
+                        dp[start][brought][cap] = max(
+                            -prices[start] + dp[start+1][1][cap],
+                            dp[start+1][0][cap]# hold
+                        )
+
+        return dp[0][0][2]
+
 s = Solution()
 # %%
