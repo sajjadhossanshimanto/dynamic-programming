b8e89b3c4fe36ea8d8b9da1c78057b28d4b2bb53
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2025-02-27 09:27:48 +0600

    recurtion solution

--- 5. dp on stock/3. iii Best Time to Buy and Sell Stock.py
+++ 5. dp on stock/3. iii Best Time to Buy and Sell Stock.py
@@ -1,5 +1,11 @@
 '''
 https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
+- leetcode hard 
+n=10^5
+so we have to do it in O(n) or O(nlogn) time
+
+but be afread overthinking that you need to implement #D dp
+3D dp doesn't mendatorily mean O(n^3). 
 '''
 #%%
 from typing import List
@@ -16,14 +22,17 @@ class Solution:
 
 s = Solution()
 # %%
+# 6
 s.maxProfit([3,3,5,0,0,3,1,4])
 # %%
+# 4
 s.maxProfit([1,2,3,4,5])
 # %%
 '''
 this tecnique will not work here.
 we may skip any down fall and wait for next bullrun to sell and maximise profit
 '''
+# 0
 s.maxProfit([7,6,4,3,1])
 # %%
 from heapq import heappush, heappop
@@ -44,6 +53,37 @@ class Solution:
             stack.append((i, profit))
         
         return ans
+
+s = Solution()
+# %%
+# continueing after a long period
+
+
+from functools import lru_cache
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        @lru_cache(None)
+        def recurtion(start=0, brought=False, cap=2):
+            if cap==0 or start==len(prices):
+                return 0# logic: run the loop till the end of list or no more scope to buy.
+
+            if brought:
+                # we have 2 choices either to sell or not
+                return max(
+                    prices[start]+recurtion(start+1, False, cap-1),# sold
+                    recurtion(start+1, True, cap)# hold
+                )
+            else:
+                # we have 2 choices either to buy or not
+                return max(
+                    -prices[start]+recurtion(start+1, True, cap),# bought. buying is not the end of transaction. so cap remains same
+                    recurtion(start+1, False, cap)# hold
+                )
+
+        return recurtion()
 
 s = Solution()
 # %%
